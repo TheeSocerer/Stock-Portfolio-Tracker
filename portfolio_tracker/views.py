a6b5fede629b832fbee0e029698ea1da8c5c4ec2
tshiburi123 <tshiburi123@student.wethinkcode.co.za>
2024-05-21 21:10:44 +0200

    Added the portfolio_tracker app and it's migrations. with url paths

---
+++ portfolio_tracker/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+# Create your views here.
+
+def portfolio(request):
+    return HttpResponse("Hello, world. This is the portfolio view.")
