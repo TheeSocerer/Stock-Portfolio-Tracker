d1f53055cc65b4157e8d7409899eea7e5c5c1033
tshiburi123 <tshiburi123@student.wethinkcode.co.za>
2024-06-18 08:41:33 +0200

    added quotes app, superuser and changed project to stock

--- stock/asgi.py
+++ stock/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for stock_portfolio_tracker project.
+ASGI config for stock project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stock_portfolio_tracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stock.settings')
 
 application = get_asgi_application()
--- stock/wsgi.py
+++ stock/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for stock_portfolio_tracker project.
+WSGI config for stock project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stock_portfolio_tracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stock.settings')
 
 application = get_wsgi_application()
