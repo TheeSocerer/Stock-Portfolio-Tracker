db103fcf3bf937919162cc2d82883b3c48dd6503
tshiburi123 <tshiburi123@student.wethinkcode.co.za>
2024-06-18 09:22:55 +0200

    installed bootstrap and created a base template

--- quotes/urls.py
+++ quotes/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from quotes import views
 
 urlpatterns = [
-    path('',views.home, name= "home")
+    path('',views.home, name= "home"),
+    path('about/', views.about, name="about")
 ]
