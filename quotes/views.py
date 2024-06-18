9eddee967b4e71acc3b16a6044180cd0f20da2ea
tshiburi123 <tshiburi123@student.wethinkcode.co.za>
2024-06-18 15:31:02 +0200

    created a table in the add_stock template. made the table show tickers and their delete link

--- quotes/views.py
+++ quotes/views.py
@@ -27,6 +27,7 @@ def home(request):
 
 
 def add_stock(request):
+    import requests
     if request.method == 'POST':
         form = StockForm(request.POST or None)
 
@@ -35,6 +36,19 @@ def add_stock(request):
             messages.success(request, ("Stock has been added"))
             return redirect('add_stock')
     else:
+        # aplhapoint= 32PUKSXOZL0C4RQD
+        # polygon.io= Yc5MSGv3chvIQNmQLtO64sVPkrDF3Yb2
+        if request.method == 'POST':
+            ticker = request.POST['ticker']
+            # modify this for the search
+            for ticker_item in ticker:
+                #convert it to a string funtion
+                api_requests = requests.get("https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2024-01-09/2024-01-09"
+                                            "?apiKey=Yc5MSGv3chvIQNmQLtO64sVPkrDF3Yb2")
+            try:
+                api = json.loads(api_requests.content)
+            except Exception as e:
+                api = "Error....."
 
         ticker = Stock.objects.all()
         return render(request, 'add_stock.html', {'ticker': ticker})
