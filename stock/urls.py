291e4c3ed43b917010315ec7615c58825853fcfa
tshiburi123 <tshiburi123@student.wethinkcode.co.za>
2024-06-18 13:45:47 +0200

    removed the stock portfolio tracker app

--- stock/urls.py
+++ stock/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quotes.urls')),
-    path('portfolio-tracker/', include('portfolio_tracker.urls')),
 ]
